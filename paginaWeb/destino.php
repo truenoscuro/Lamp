5af46bec8da68a3a6ecd61be4c31ac5e4e171762
Nofre Ruiz <nofreruizsalom@gmail.com>
2022-09-28 10:28:58 +0200

    arrays i paso Variables

---
+++ paginaWeb/destino.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Plantilla</title>
+</head>
+<body>
+    <?php 
+    
+    echo "Nombre " . $_GET["nombre"];
+    echo "<p> <p>";
+    echo "Carreg " . $_GET["cargo"];
+    
+    
+    ?>    
+</body>
+</html>
